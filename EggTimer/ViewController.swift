171f532bd363168aaa31a312ad551fbc0461c6c0
M.Taimoor Hassan <mtaimoor.tbox@gmail.com>
2023-03-07 16:22:08 +0500

    EggBoiler
    EggBoiler App

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -6,42 +6,62 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     // MARK: OUTLETS
     
     @IBOutlet weak var eggStatusLabl: UILabel!
+    @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     // MARK: VARIABLES
-    var softTime = 5
-    var mediumTime = 7
-    var hardTime = 12
-    let softEgg = "Soft"
-    let medEgg = "Medium"
+    var eggTimes : [String:Int] = ["Soft":10,"Medium":20,"Hard":30]
+    var timer = Timer()
+    var totalTime = 0
+    var secPassed = 0
+    var player: AVAudioPlayer!
     
     // MARK: viewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        textLabel.text = "Working On It to Change Label "
+        
+        progressBar.progress = 0.0
+        progressBar.layer.cornerRadius = 5
+        
     }
     
     // MARK: eggPressed [Button]
     @IBAction func eggPressed(_ sender: UIButton) {
-        let hardness = sender.titleLabel?.text
-       
-        if let hardness {
-            print(hardness)
-        } else {
-            print("Nothing to Print")
-        }
+        timer.invalidate()
+        guard let hardness = sender.titleLabel?.text else { return }
+        textLabel.text = hardness
         
-       
-        if hardness == softEgg {
-            print ("The time For boiling Egg is: \(softTime) Minutes")
-        } else if hardness == medEgg {
-            print ("The time For boiling Egg is: \(mediumTime) Minutes")
-        } else { print ("The time For boiling Egg is: \(hardTime) Minutes")
+        totalTime = eggTimes[hardness]!
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [self] (Timer) in
             
+            if secPassed <= totalTime  {
+                
+                print ("\(self.secPassed) seconds.")
+                
+                self.textLabel.text = "\(self.secPassed) seconds."
+                
+                progressBar.progress = Float(secPassed) / Float(totalTime)
+                
+                self.secPassed += 1
+                
+            } else {
+                Timer.invalidate()
+                self.textLabel.text = "Done! Egg is Ready."
+                
+                let url = Bundle.main.url(forResource: "anda", withExtension: "mp3")
+                   player = try! AVAudioPlayer(contentsOf: url!)
+                   player.play()
+            }
         }
     }
 }
